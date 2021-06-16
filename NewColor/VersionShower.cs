2973ec81c4ba452c789757e90196ae1b0eabe9c4
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2021-06-16 21:03:43 +0200

    Hardel API Update
    Update

---
+++ NewColor/VersionShower.cs
@@ -0,0 +1,11 @@
+﻿using HardelAPI;
+using HarmonyLib;
+namespace NewColor {
+    
+    [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
+    public static class VersionShowerPatch {
+        public static void Postfix() {
+            HarionVersionShower.Text.text += "\n<color=#9c1919FF>New</color> <color=#FFDD12FF>Col</color><color=#00ACEDFF>or</color>";
+        }
+    }
+}
