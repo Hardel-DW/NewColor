e56a68f8cbbd9c8e6019ba2cf03c6a81d149cd03
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2021-06-21 02:12:31 +0200

    Update to Harion 1.3

--- NewColor/NewColor.cs
+++ NewColor/NewColor.cs
@@ -6,18 +6,18 @@ using UnityEngine;
 using System.Reflection;
 using System.Linq;
 using System.Collections.Generic;
-using HardelAPI.Utility.Helper;
-using HardelAPI;
-using static HardelAPI.Utility.Helper.ColorHelper;
-using static HardelAPI.Utility.HatsCreator;
-using HardelAPI.ModsManagers.Configuration;
-using HardelAPI.ModsManagers.Mods;
+using Harion;
+using Harion.Utility.Helper;
+using Harion.ModsManagers.Configuration;
+using Harion.ModsManagers.Mods;
+using Harion.ColorDesigner;
+using Harion.HatDesigner;
 
 namespace NewColor {
 
     [BepInPlugin(Id)]
     [BepInProcess("Among Us.exe")]
-    [BepInDependency(HardelApiPlugin.Id)]
+    [BepInDependency(HarionPlugin.Id)]
     public class NewColor : BasePlugin, IModManager, IModManagerLink {
         public const string Id = "fr.evan.newcolor";
         public static ManualLogSource Logger;
@@ -26,7 +26,7 @@ namespace NewColor {
 
         public string DisplayName => "New Color";
 
-        public string Version => typeof(HardelApiPlugin).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        public string Version => typeof(NewColor).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
 
         public string SmallDescription => "Add skin and hat";
 
@@ -46,61 +46,61 @@ namespace NewColor {
         }
 
         private void AddColor() {
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Dark Red",
                 color = new Color32(156, 25, 25, byte.MaxValue),
                 shadow = new Color32(80, 0, 0, byte.MaxValue),
                 isLighterColor = false
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Yellow",
                 color = new Color32(255, 221, 18, byte.MaxValue),
                 shadow = new Color32(179, 145, 0, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Light Blue",
                 color = new Color32(0, 172, 237, byte.MaxValue),
                 shadow = new Color32(0, 92, 161, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Light Rose",
                 color = new Color32(242, 198, 209, byte.MaxValue),
-                shadow = ColorHelper.Shadow(new Color32(242, 198, 209, byte.MaxValue)),
+                shadow = new Color32(172, 128, 139, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Turquoise",
                 color = new Color32(22, 132, 176, byte.MaxValue),
                 shadow = new Color32(15, 89, 117, byte.MaxValue),
                 isLighterColor = false
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Gray",
                 color = new Color32(175, 175, 175, byte.MaxValue),
-                shadow = ColorHelper.Shadow(new Color32(175, 175, 175, byte.MaxValue)),
+                shadow = new Color32(105, 105, 105, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Black",
                 color = new Color32(30, 30, 30, byte.MaxValue),
                 shadow = new Color32(10, 10, 10, byte.MaxValue),
                 isLighterColor = false
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "White",
                 color = new Color32(240, 240, 240, byte.MaxValue),
-                shadow = ColorHelper.Shadow(new Color32(240, 240, 240, byte.MaxValue)),
+                shadow = new Color32(170, 170, 170, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Dark Green",
                 color = new Color32(15, 89, 7, byte.MaxValue),
                 shadow = new Color32(5, 30, 0, byte.MaxValue),
                 isLighterColor = true
             });
-            ColorHelper.AddColor(new CustomColor {
+            ColorCreator.AddColor(new CustomColor {
                 name = "Turquoise",
                 color = new Color32(61, 255, 181, byte.MaxValue),
                 shadow = new Color32(31, 128, 91, byte.MaxValue),
@@ -113,7 +113,7 @@ namespace NewColor {
             List<string> HatFiles = fiesNames.Where(item => item.EndsWith(".png")).ToList();
 
             foreach (string hat in HatFiles) {
-                CreateHats(new HatData {
+                HatCreator.CreateHats(new CustomHat {
                     sprite = SpriteHelper.LoadHatSprite(hat),
                     bounce = false,
                     highUp = false,
--- NewColor/VersionShower.cs
+++ NewColor/VersionShower.cs
@@ -1,4 +1,4 @@
-﻿using HardelAPI;
+﻿using Harion;
 using HarmonyLib;
 namespace NewColor {
     
